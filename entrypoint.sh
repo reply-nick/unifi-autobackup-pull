0d66952b6f3ebc99c5ae0da0b8ee3433c9d6c53a
Nicolae Suruceanu <nick.spado@slmail.me>
2026-05-27 13:36:23 -0500

    Fix setpgid permission error by running crond in subshell

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,4 +19,4 @@ echo "$CRON_SCHEDULE /app/pull-unifi-backup.sh" | crontab -
 echo "Cron schedule set to: $CRON_SCHEDULE"
 
 # Run cron in foreground (foreground mode for Docker)
-exec crond -f -l 8
+exec bash -c 'crond -f -l 8 & wait'
